c8f6c98b088f2fd8be8ad7ac89f5f94a986b7171
洪恒辉 <100217283+akalinh@users.noreply.github.com>
2024-01-28 20:35:53 +0800

    Delete AshukaGame directory

--- AshukaGame/src/main/java/com/frame/FrameConstants.java
+++
@@ -1,9 +0,0 @@
-package com.frame;
-
-public class FrameConstants {
-	public static final int X_LOCATION = 200;
-	public static final int Y_LOCATION = 100;
-	public static final int WIDTH = 800;
-	public static final int HEIGHT =800;
-	public static final String TITLE = "Game Frame";
-}
